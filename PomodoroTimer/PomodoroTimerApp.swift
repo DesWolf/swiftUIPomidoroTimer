ffafcf3584d90aab320ebb33c20127ad1437e5a1
MaxOkuneev <okuneev@trend.tech>
2023-01-23 17:51:29 +0300

    Initial Commit

---
+++ PomodoroTimer/PomodoroTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroTimerApp.swift
+//  PomodoroTimer
+//
+//  Created by Максим Окунеев on 23.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
